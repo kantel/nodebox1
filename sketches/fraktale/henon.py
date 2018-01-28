9c7a36d5005ff7f46cdfe0b94c7f9d540b96769d
Jörg Kantel <joerg@kantel.de>
2018-01-28 15:02:32 +0100

    Henon-Abbildung

---
+++ sketches/fraktale/henon.py
@@ -0,0 +1,41 @@
+import math
+
+size(500, 500)
+
+# Konstanten
+a = 1.5732
+# a = 1.35
+h = 0.02
+max = 10.0e+9
+
+x0 = 0.01
+y0 = -0.02
+cosa = math.cos(a)
+sina = math.sin(a)
+
+fill(0.9)
+rect(0, 0, WIDTH, HEIGHT)
+x = x0
+y = y0
+for j in range(50):
+    for i in range(1500):
+        if (x < max) and (y < max):
+            x1 = x*cosa - (y - x*x)*sina
+            y = x*sina + (y - x*x)*cosa
+            x = x1
+            if (abs(x + 1.3) < 3) and (abs(1.3 - y) < 3):
+                p = (x + 1.3)*180
+                q = (1.3 - y)*180
+                if (i < 800):
+                    fill(0.9, 0, 0)
+                elif (i < 1200):
+                    fill(0, 0.9, 0)
+                else:
+                    fill(0, 0, 0.9)
+                oval(p, q, 2, 2)
+    x = x0 + j*h
+    y = y0 + j*h
+
+fill(0)
+fontsize(14)
+text("Quadratische Henon-Gleichung für a = 1.5732", 20, WIDTH - 20)
