5ca854dab2ba71bae60ace90fba88e07a8c895bb
Jörg Kantel <joerg@kantel.de>
2018-01-29 20:31:23 +0100

    Farben geändert

--- sketches/fraktale/henon.py
+++ sketches/fraktale/henon.py
@@ -1,6 +1,7 @@
 import math
 
 size(500, 500)
+colormode(RGB, range = 255)
 
 # Konstanten
 a = 1.5732
@@ -13,7 +14,7 @@ y0 = -0.02
 cosa = math.cos(a)
 sina = math.sin(a)
 
-fill(0.9)
+fill(20)
 rect(0, 0, WIDTH, HEIGHT)
 x = x0
 y = y0
@@ -25,17 +26,23 @@ for j in range(50):
             x = x1
             p = (x + 1.25)*180
             q = (1.4 - y)*180
-            if (i < 800):
-                fill(0.9, 0, 0)
-            elif (i < 1200):
-                fill(0, 0.9, 0)
+            if (i < 250):
+                fill(183, 116, 64)
+            elif (i < 500):
+                fill(17, 42, 106)
+            elif (i < 750):
+                fill(159, 53, 233)
+            elif (i < 1000):
+                fill(57, 218, 56)
+            elif (i < 1250):
+                fill(67, 254, 211)
             else:
-                fill(0, 0, 0.9)
+                fill(212, 251, 69)
             oval(p, q, 2, 2)
     x = x0 + j*h
     y = y0 + j*h
 
-fill(0)
+fill(255)
 fontsize(14)
 msg = "Quadratische Henon-Gleichung für a = {}".format(a)
 text(msg, 20, WIDTH - 20)
