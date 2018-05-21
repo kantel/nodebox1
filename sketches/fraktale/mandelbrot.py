79bdc0a15f2fe054372473f8378bee16eff2529f
Jörg Kantel <joerg@kantel.de>
2018-05-21 20:42:46 +0200

    Mandelbrot cont.

--- sketches/fraktale/mandelbrot.py
+++ sketches/fraktale/mandelbrot.py
@@ -1,6 +1,6 @@
 coreimage = ximport("coreimage")
 
-size(500, 500)
+size(300, 300)
 
 canvas = coreimage.canvas(WIDTH, HEIGHT)
 
@@ -17,10 +17,16 @@ def mandelbrot(x, y, depth = 64):
 pixels = []
 w = WIDTH
 h = HEIGHT
+left   = -2.25
+right  = 0.75
+bottom = -1.5
+top    = 1.5
 
 for i in range(w):
+    cr = left + i*(right - left)/w
     for j in range(h):
-        v = mandelbrot(float(i)/w, float(j)/h)
+        ci = bottom + j*(top - bottom)/h
+        v = mandelbrot(cr, ci)
         pixels.append(color(v/10.0, v/20.0, v/10.0))
 
 l = canvas.append(pixels, w, h)
