20f51b9fd30a4c86cd0d96b7636db75596b504cf
Jörg Kantel <joerg@kantel.de>
2022-03-15 18:59:51 +0100

    Create hellonodebox11.py

---
+++ sketches/hellonodebox/hellonodebox11.py
@@ -0,0 +1,10 @@
+size(300, 400)
+
+print("Hallo, Jörg! Ich mag 🐍")
+
+colormode(RGB, range = 255)
+stroke(0)
+fill(250, 100, 100, 125)
+rect(30, 30, 200, 200)
+fill(100, 150, 200, 125)
+rect(50, 50, 220, 220)
